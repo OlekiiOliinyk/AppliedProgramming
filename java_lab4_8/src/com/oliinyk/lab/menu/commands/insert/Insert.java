21e7974384ff6ea5ae3cd8037a5a380884fabc88
OleksiiOliinyk <112064559+OlekiiOliinyk@users.noreply.github.com>
2022-11-09 12:36:17 +0200

    Delete java_lab4_8 directory

--- java_lab4_8/src/com/oliinyk/lab/menu/commands/insert/Insert.java
+++
@@ -1,9 +0,0 @@
-package com.oliinyk.lab.menu.commands.insert;
-
-import com.oliinyk.lab.insurance.BasicInsurance;
-
-import java.util.List;
-
-public interface Insert {
-    void execute(List<BasicInsurance> derevative);
-}
